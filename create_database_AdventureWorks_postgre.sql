a64d7099528a8844a9f22df3d2049820f937b9e2
Thus Vang <thusvang@gmail.com>
2025-05-02 02:37:37 -0500

    first commit

---
+++ create_database_AdventureWorks_postgre.sql
@@ -0,0 +1,14 @@
+-- Database: AdventureWorks
+
+-- DROP DATABASE IF EXISTS "AdventureWorks";
+
+CREATE DATABASE "AdventureWorks"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en-US'
+    LC_CTYPE = 'en-US'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;

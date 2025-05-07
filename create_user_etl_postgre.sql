a905567bad9ec788ada0e43734b2f9030de3ff13
Thus Vang <thusvang@gmail.com>
2025-05-07 04:11:16 -0500

    removed sql scripts

--- create_user_etl_postgre.sql
+++
@@ -1,8 +0,0 @@
---create ETL user
-CREATE USER etl WITH PASSWORD 'etlpass';
---grant connect
-GRANT CONNECT ON DATABASE "AdventureWorks" TO etl;
---grant table permissions
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO etl;
-GRANT USAGE ON SCHEMA public TO etl;
-GRANT CREATE ON SCHEMA public TO etl;
